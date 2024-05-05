d8f541c861c18c1b25a013cb8a4af2de5aeadfd9
piovke <limek008@gmail.com>
2024-05-05 20:48:16 +0200

    wyszukiwanie artysow. szukasz sab dostajesz Sabrina Carpenter Sabaton Sabrina Claudio Saba

---
+++ user top songs.py
@@ -0,0 +1,43 @@
+from dotenv import load_dotenv
+import os
+import base64
+from requests import post, get
+
+load_dotenv()
+
+client_id = os.getenv("CLIENT_ID")
+client_secret = os.getenv("CLIENT_SECRET")
+
+
+def get_token():
+    auth_string = client_id + ':' + client_secret
+    auth_utf = auth_string.encode("utf-8")
+    auth_base64 = str(base64.b64encode(auth_utf), "utf-8")
+
+    url = "https://accounts.spotify.com/api/token"
+    headers = {
+        "Authorization": "Basic " + auth_base64,
+        "Content-Type": "application/x-www-form-urlencoded"
+    }
+    data = {"grant_type": "client_credentials"}
+    result = post(url, headers=headers, data=data)
+    json_result = result.json()
+    token = json_result["access_token"]
+    return token
+
+
+def get_auth_header(token):
+    return {"Authorization": "Bearer " + token}
+
+
+def get_user(token):
+    url = "https://api.spotify.com/v1/me"
+    header = get_auth_header(token)
+    result = get(url, headers=header)
+    return result
+
+
+token = get_token()
+print(token)
+user = get_user(token).json()
+print(user)
